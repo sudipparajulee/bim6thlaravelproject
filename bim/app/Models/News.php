d85c671160b530fa532cd375eac178881d031531
Sudip Parajuli <sudip.parajulee@gmail.com>
2023-05-07 09:54:11 +0545

    Baisakh 24

--- bim/app/Models/News.php
+++ bim/app/Models/News.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
+    // protected $table = "mynews"; // if table name is different from model name
     protected $guarded = [];
 
     function category()
